b35f7fbda6f0995c52aa3de74091e1d2a7e4800d
shafik720 <shafikrasel5@gmail.com>
2021-08-20 21:15:25 +0600

    decrease done

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 
-document.getElementById('case-button').addEventListener('click',function(){
-    let caseNumbers = document.getElementById('case-number');
+// increament
+function increaseNumber(inputFieldId){
+    let caseNumbers = document.getElementById(inputFieldId);
     let caseNumber = parseInt(caseNumbers.value) ;
     
     let showNumber = caseNumber +1;
-    caseNumbers.value = showNumber;
+    return caseNumbers.value = showNumber;
+}
+document.getElementById('case-button').addEventListener('click',function(){
     
+    increaseNumber('case-number');
 })
 
+
+// decreament
+function decreaseNumber(inputFieldId){
+    let caseNumbers = document.getElementById(inputFieldId);
+    let caseNumber = parseInt(caseNumbers.value) ;
+    
+    let showNumber = caseNumber -1;
+    return caseNumbers.value = showNumber;
+}
+document.getElementById('case-button-minus').addEventListener('click', function(){
+    decreaseNumber('case-number');
+})
