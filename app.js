4df64d59d6a16087ba41d0753ddc35e6d2209fdf
shafik720 <shafikrasel5@gmail.com>
2021-08-20 23:59:17 +0600

    sdkcfh

--- app.js
+++ app.js
@@ -14,13 +14,24 @@ function decreaseNumber(inputFieldId){
     if(caseNumber>0){
         let showNumber = caseNumber - 1;
         return caseNumbers.value = showNumber;
-    }
-    
+    }    
 }
 
 // to increase or decrease case amount
+
+
+
+
 document.getElementById('case-button').addEventListener('click',function(){    
     increaseNumber('case-number');
+    let caseAmountss = document.getElementById('case-number');
+    let caseAmounts = caseAmountss.value;
+    let caseAmount = parseInt(caseAmounts);
+    let casePrice = 59 * caseAmount;
+    let showCasePrice = document.getElementById('fifty-nine');
+    showCasePrice.innerHTML = casePrice;
+    console.log(casePrice);
+
 })
 document.getElementById('case-button-minus').addEventListener('click', function(){
     decreaseNumber('case-number');
