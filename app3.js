e281fc31c788ba2d6e424243293d5cafc0d79aad
shafik720 <shafikrasel5@gmail.com>
2021-08-21 13:08:22 +0600

    sdsfs

--- app3.js
+++ app3.js
@@ -1,33 +1,34 @@
 
-function increaseProduct(productName, isTrue){
-    
-    if(isTrue == true){
+function increaseProduct(productName, isTrue, price){
         let inputTexts = document.getElementById(productName+'-number');
         let inputText = parseInt(inputTexts.value);
+
+    if(isTrue == true){        
         let amounts = inputText + 1;
         inputTexts.value = amounts;
         
-    }else  {
-        let inputTexts = document.getElementById(productName+'-number');
-        let inputText = parseInt(inputTexts.value);
-        if(parseInt(inputTexts.value)>0){            
+    }else if(parseInt(inputTexts.value)>0)  {                 
         let amounts = inputText - 1;
-        inputTexts.value = amounts;
-        }
+        inputTexts.value = amounts;        
     }
+
+    let mobileValues = document.getElementById(productName+'-value');
+    mobileValues.innerText = parseInt(inputTexts.value) * price;
 }
+
+
 //for mobile phone
 document.getElementById('mobile-button').addEventListener('click', function(){
-    increaseProduct('mobile', true);
+    increaseProduct('mobile', true, 1219);
 })
 document.getElementById('mobile-button-minus').addEventListener('click', function(){
-    increaseProduct('mobile', false);
+    increaseProduct('mobile', false, 1219);
 })
 
 // for case 
 document.getElementById('case-button').addEventListener('click', function(){
-    increaseProduct('case', true);
+    increaseProduct('case', true, 59);
 })
 document.getElementById('case-button-minus').addEventListener('click', function(){
-    increaseProduct('case', false);
+    increaseProduct('case', false, 59);
 })
